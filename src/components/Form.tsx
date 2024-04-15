0d94853f917e2239db2332be3c90e25c1889b31f
sandboa <bsse0807@iit.du.ac.bd>
2024-04-15 15:07:48 +0600

    schema based validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,49 +1,44 @@
 import {FieldValues, useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
 
-interface FormData {
-    name: string;
-    age: number;
-}
+const schema = z.object({
+    name: z.string().min(3, {message: 'Name is too short. It should have at least 3 characters'}),
+    age: z.number({invalid_type_error: 'Age field is required'}).int().min(10, {message: 'Age should be at least 10'}).max(99, {message: 'Age should be at most 99'}),
+});
 
+type FormData = z.infer<typeof schema>; // { name: string; age: number; }
 const Form = () => {
 
-    const {register, handleSubmit, formState} = useForm<FormData>();
+    const {register, handleSubmit, formState} = useForm<FormData>({resolver: zodResolver(schema)});
     // console.log(formState);
     const onSubmit = (data: FieldValues) => console.log(data);
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-3"><label htmlFor={"name"} className="form-label">Name</label>
-                <input
-                    {...register('name', {required: true, minLength: 3})}
-                    id={"name"}
-                    type={"text"}
-                    className="form-control"
-                />
-                <p>
-                    {formState.errors.name?.type === 'required' && <p className={"text-danger"}>'Name is required'</p>}
-                    {formState.errors.name?.type === 'minLength' &&
-                        <p className={"text-danger"}>'Name is too short. It should have at least 3 characters'</p>}
-                </p>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="age" className="form">Age</label>
-                <input
-                    {...register('age', {required: true, minLength: 2, maxLength: 2})}
-                    id="age" type="number"
-                    className="form-control"
-                />
-                <p>
-                    {formState.errors.age?.type === 'required' && <p className={"text-danger"}>'Age is required' </p>}
-                    {formState.errors.age?.type === 'minLength' &&
-                        <p className={"text-danger"}>'Age should be at least 10'</p>}
-                    {formState.errors.age?.type === 'maxLength' &&
-                        <p className={"text-danger"}>'Age should be at most 99'</p>}
-                </p>
-            </div>
-            <button className="btn btn-primary" type={"submit"}>Submit</button>
-        </form>
-    );
+    return (<form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3"><label htmlFor={"name"} className="form-label">Name</label>
+            <input
+                {...register('name')}
+                id={"name"}
+                type={"text"}
+                className="form-control"
+            />
+            <p>
+                {formState.errors.name && (<p className={"text-danger"}>{formState.errors.name.message}</p>)}
+            </p>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="age" className="form">Age</label>
+            <input
+                {...register('age', {valueAsNumber: true})}
+                id="age" type="number"
+                className="form-control"
+            />
+            <p>
+                {formState.errors.age && (<p className={"text-danger"}>{formState.errors.age.message}</p>)}
+            </p>
+        </div>
+        <button className="btn btn-primary" type={"submit"}>Submit</button>
+    </form>);
 }
 
 export default Form;
