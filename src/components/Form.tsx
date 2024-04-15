8ab78f1ca2956d3b1de3ce30867785e1c5a035c5
sandboa <bsse0807@iit.du.ac.bd>
2024-04-15 14:39:31 +0600

    applying validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,45 @@
-import {useState} from "react";
+import {FieldValues, useForm} from "react-hook-form";
+
+interface FormData {
+    name: string;
+    age: number;
+}
 
 const Form = () => {
 
-    const [person, setPerson] = useState({
-        name: '',
-        age: ''
-    });
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(person);
-    }
+    const {register, handleSubmit, formState} = useForm<FormData>();
+    // console.log(formState);
+    const onSubmit = (data: FieldValues) => console.log(data);
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3"><label htmlFor={"name"} className="form-label">Name</label>
                 <input
-                    onChange={(event) =>
-                        setPerson({
-                            ...person,
-                            name: event.target.value
-                        })
-                    }
-                    value={person.name}
-                    id={"name"} type={"text"} className="form-control"/>
+                    {...register('name', {required: true, minLength: 3})}
+                    id={"name"}
+                    type={"text"}
+                    className="form-control"
+                />
+                <p>
+                    {formState.errors.name?.type === 'required' && <p className={"text-danger"}>'Name is required'</p>}
+                    {formState.errors.name?.type === 'minLength' &&
+                        <p className={"text-danger"}>'Name is too short. It should have at least 3 characters'</p>}
+                </p>
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form">Age</label>
-                <input onChange={(event) =>
-                    setPerson({
-                        ...person,
-                        age: event.target.value
-                    })
-                } value={person.age} id="age" type="number" className="form-control"/>
+                <input
+                    {...register('age', {required: true, minLength: 2, maxLength: 2})}
+                    id="age" type="number"
+                    className="form-control"
+                />
+                <p>
+                    {formState.errors.age?.type === 'required' && <p className={"text-danger"}>'Age is required' </p>}
+                    {formState.errors.age?.type === 'minLength' &&
+                        <p className={"text-danger"}>'Age should be at least 10'</p>}
+                    {formState.errors.age?.type === 'maxLength' &&
+                        <p className={"text-danger"}>'Age should be at most 99'</p>}
+                </p>
             </div>
             <button className="btn btn-primary" type={"submit"}>Submit</button>
         </form>
