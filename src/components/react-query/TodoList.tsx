1f7a38ed4e30b38eb62c7917e4e4bc14069ba12e
sandboa <bsse0807@iit.du.ac.bd>
2024-04-17 00:12:13 +0600

    partial commit

---
+++ src/components/react-query/TodoList.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import {useQuery} from "@tanstack/react-query";
+
+interface Todo {
+    id: number;
+    title: string;
+    userId: number;
+    completed: boolean;
+}
+
+const TodoList = () => {
+    const fetchTodos =  () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => res.data)
+    }
+    useQuery({
+        queryKey: ['todos'],
+        queryFn: fetchTodos ,
+    })
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/todos')
+            .then((res) => setTodos(res.data))
+            .catch((error) => setError(error));
+    }, []);
+
+    if (error) return <p>{error}</p>;
+
+    return (
+        <ul className="list-group">
+            {todos.map((todo) => (
+                <li key={todo.id} className="list-group-item">
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default TodoList;
