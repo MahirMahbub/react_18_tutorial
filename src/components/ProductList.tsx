71c41d688e918d9978b781910e07ae766d8b100b
sandboa <bsse0807@iit.du.ac.bd>
2024-04-16 13:48:00 +0600

    add "creating a custom data fetching hook"

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,47 +1,17 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 // import axios, {CanceledError} from "axios";
-import {CanceledError} from "../services/api-client.ts";
-import userService from "../services/user-service.ts";
 
-interface Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-}
+import useProducts from "../hooks/useProducts.ts";
+
+
 
 export const ProductList = () => {
-    const [products, setProducts] = useState<Product[]>([])
+    const {products, error, loading, setProducts, setError} = useProducts();
     const [selectedCategory, setSelectedCategory] = useState("All Items");
-    const [error, setError] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
-    // useEffect(() => {
-    //     fetch('https://fakestoreapi.com/products')
-    //         .then(response => response.json())
-    //         .then(data => setProducts(data));
-    // }, []);
-    // Create an axios call to fetch the data from the API
-    useEffect(() => {
-        setLoading(true);
-        const {request, cancel} = userService.gets<Product>()
-        request.then(response => setProducts(response.data)).then(() => setLoading(false))
-            .catch(error => {
-                if (error instanceof CanceledError) {
-                    return
-                } else
-                    setError(error.message)
-                setLoading(false)
-            });
-        return () => cancel();
-    }, []);
     const uniqueCategories: string[] = products ? ["All Items", ...new Set(products.map(product => product.category))] : [];
 
     const visibleProducts = selectedCategory !== "All Items" ? products.filter(p => p.category === selectedCategory) : products;
-    // if (error) {
-    //
-    // }
+
     return (
         <div>
 
---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+import userService from "../services/user-service.ts";
+import {CanceledError} from "../services/api-client.ts";
+
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+}
+const useProducts = () => {
+    const [products, setProducts] = useState<Product[]>([])
+    const [error, setError] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    useEffect(() => {
+        setLoading(true);
+        const {request, cancel} = userService.gets<Product>()
+        request.then(response => setProducts(response.data)).then(() => setLoading(false))
+            .catch(error => {
+                if (error instanceof CanceledError) {
+                    return
+                } else
+                    setError(error.message)
+                setLoading(false)
+            });
+        return () => cancel();
+    }, []);
+    return {products, error, loading, setProducts, setError};
+}
+export default useProducts;
